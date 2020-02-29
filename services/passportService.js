494c80f622dc35bc97ca8e63d9b67ed399a58782
mehmetak <mehmetak78@hotmail.com>
2020-02-29 16:12:42 +0300

    First Commit

--- services/passportService.js
+++ services/passportService.js
@@ -3,9 +3,15 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const DB = require("../IN_MEMORY_DB");
 
+const keys = require("../config/keys");
 
 
 console.log(DB.USER_TABLE);
 
-passport.use(new GoogleStrategy("http://localhost:5000/auth/google/callback)"));
+passport.use(new GoogleStrategy({
+                                    clientID: keys.googleClientID,
+                                    clientSecret: keys.googleClientSecret,
+                                    callbackURL: "/auth/google/callbacknow",
+                                    proxy:true
+                                }));
 
