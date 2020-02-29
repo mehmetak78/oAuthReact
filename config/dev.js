5dee02a0ee541e26a081d4af3e6fa48dd2038343
mehmetak <mehmetak78@hotmail.com>
2020-02-29 16:09:38 +0300

    First Commit

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+    googleClientID: "733954831037-gp3c3qthj0nor87o3vcehp0fccekse79.apps.googleusercontent.com",
+    googleClientSecret: "rWri_UtOikIBdWBNBRV_dbLp",
+
+    mongoURI: "mongodb://localhost:27017/emaily-dev",
+    cookieKey: "jksdfkjhlanljadfaıhfajsdfabnvasjdfhşasfşkasjfhlasjf",
+    stripePublishableKey: "pk_test_lvBC407J5nkP06cTi3MFHmFT",
+    stripeSecretKey: "sk_test_Qq1ds86opFckLdV1VfqsfAo7",
+    sendGridKey: "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",
+    redirectDomain: "http://localhost:3000"
+};
+
+//ff
